b6b5eeecfeb81ef72b1e748032004b07ae9bfe54
joao pedro <joao.albuquerque@irede.org.br>
2024-01-24 15:46:54 -0300

    testing

--- models/browser.py
+++ models/browser.py
@@ -4,13 +4,15 @@ import robocorp.workitems
 class Browser:
     def __init__(self, path: str) -> None:
         
-        inputs  = robocorp.workitems.Inputs()
-        item    = inputs.reserve()  
-        payload = item.payload
+        # inputs  = robocorp.workitems.Inputs()
+        # item    = inputs.reserve()  
+        # payload = item.payload
 
         self.site:str = 'https://www.nytimes.com/'
         
         try:
             browser.goto(self.site)
+            with open("teste.txt","w") as file:
+                file.write("Hello Robocloud!")
         except:
             print("Error on open browser")
