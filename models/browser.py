8f0738b9521ddd7ad600ce023233ca56149b9534
Pedro <joao.pedro73210@gmail.com>
2024-01-24 17:35:41 -0300

    teste output

--- models/browser.py
+++ models/browser.py
@@ -8,11 +8,25 @@ class Browser:
         # item    = inputs.reserve()  
         # payload = item.payload
 
-        self.site:str = 'https://www.nytimes.com/'
+        self.site:str = 'https://www.google.com/'
         
         try:
-            browser.goto(self.site)
-            with open("teste.txt","w") as file:
-                file.write("Hello Robocloud!")
+            # browser.goto(self.site)
+            # with open("teste.txt","w") as file:
+            #     file.write("Hello Robocloud!")
+            inputs = robocorp.workitems.Inputs()
+
+            # Reserve um workitem da fila
+            item = inputs.reserve()
+
+            # Acesse o payload do workitem
+            payload = item.payload
+
+            # Crie um novo arquivo TxR
+            with open("hello_robocorp.txr", "w") as f:
+                f.write("hello robocorp")
+
+            # Libere o workitem da fila
+            inputs.release(item)
         except:
             print("Error on open browser")
