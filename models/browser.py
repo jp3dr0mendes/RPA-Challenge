5db60ce81a86f2a741ad112dbcc5453d2ee66a2b
joao pedro <joao.albuquerque@irede.org.br>
2024-01-25 13:42:23 -0300

    output

--- models/browser.py
+++ models/browser.py
@@ -3,30 +3,10 @@ import robocorp.workitems
 
 class Browser:
     def __init__(self, path: str) -> None:
-        
-        # inputs  = robocorp.workitems.Inputs()
-        # item    = inputs.reserve()  
-        # payload = item.payload
-
-        self.site:str = 'https://www.google.com/'
+        # self.site:str = 'https://www.nytimes.com/'
+        self.site:str = 'https://www.google.com'
         
         try:
-            # # browser.goto(self.site)
-            # # with open("teste.txt","w") as file:
-            # #     file.write("Hello Robocloud!")
-            # inputs = robocorp.workitems.Inputs()
-
-            # # Reserve um workitem da fila
-            # item = inputs.reserve()
-
-            # # Acesse o payload do workitem
-            # payload = item.payload
-
-            # Crie um novo arquivo TxR
-            with open("teste.txt", "w") as f:
-                f.write("hello robocorp")
-
-            # Libere o workitem da fila
-            # inputs.release(item)
+            browser.goto(self.site)
         except:
             print("Error on open browser")
