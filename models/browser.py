bba7394394eb4f690239a0a68b41852932efe5b0
joao pedro <joao.albuquerque@irede.org.br>
2024-01-25 15:51:58 -0300

    start scrapper

--- models/browser.py
+++ models/browser.py
@@ -2,11 +2,15 @@ from robocorp import browser
 import robocorp.workitems
 
 class Browser:
-    def __init__(self, path: str) -> None:
-        # self.site:str = 'https://www.nytimes.com/'
-        self.site:str = 'https://www.google.com'
+    def __init__(self) -> None:
+        self.site:str = 'https://www.nytimes.com/'
+        # self.site:str = 'https://www.google.com'
         
         try:
             browser.goto(self.site)
+            page = browser.page()
+            
+            page.fill("#searchTextField","test")
+            page.click(".css-1bf34ii")
         except:
             print("Error on open browser")
