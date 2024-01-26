ee4ee78ad3f971a445ea21a1a7c8742f06aec5d2
joao pedro <joao.albuquerque@irede.org.br>
2024-01-26 16:25:21 -0300

    lkjsd

--- models/browser.py
+++ models/browser.py
@@ -14,6 +14,8 @@ class Browser():
             slowmo=100,
         )
 
+        time.sleep(random.randint(1,4))
+
         self.page = browser.page()
 
         try:
@@ -30,11 +32,17 @@ class Browser():
 
         self.page.click("data-testid=search-button")
         self.page.fill(".css-1u4s13l", "oi")
-        self.page.click("button:text('Accept all')")
         self.page.click("button:text('Go')")
         self.page.click("data-testid=search-date-dropdown-a")
+        self.page.click("button:text('Specific Dates')")
 
         self.filter_time(month)
+
+        time.sleep(5)
+
+        self.page.click("data-testid=search-page-submit")
+        self.page.click("data-testid=search-page-submit")
+        self.page.click("data-testid=search-page-submit")
 
     def filter_time(self, month: int) -> None:
 
@@ -43,20 +51,24 @@ class Browser():
         else:
             date_user = datetime.now() - timedelta(days=30*month)
 
-        self.page.fill("#startDate", "{date_user.month}/01/{date_user.year}")
+        self.page.fill(f"#startDate", f"{date_user.month}/01/{date_user.year}")
 
 
         if date_user.month == 2:
 
             if datetime.now().year // 4 != 0:
-                self.page.fill("#endDate", "28/{date_user.month}/28/2024")
+                self.page.fill("#endDate", f"28/{date_user.month}/28/2024")
+                self.page.click(f"text('28')")
             else:
-                self.page.fill("#endDate", "29/{date_user.month}/29/2024")
+                self.page.fill("#endDate", f"29/{date_user.month}/29/2024")
+                self.page.click(f"text('29')")
 
         elif date_user.month in [1,3,5,7,8,10,12]:
-            self.page.fill("#endDate", "{date_user.month}/31/2024")
+            self.page.fill("#endDate", f"{date_user.month}/31/2024")
+            self.page.click(f"text('31')")
         else:
-            self.page.fill("#endDate", "{date_user.month}/30/2024")
+            self.page.fill("#endDate", f"{date_user.month}/30/2024")
+            self.page.click(f"text('30')")
 
 # class Crawler:
 
