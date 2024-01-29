1bc5caf5a1df6bc0f65b152b12ef19f3ea71ba92
Pedro <joao.pedro73210@gmail.com>
2024-01-29 17:43:06 -0300

    testai

--- tasks.py
+++ tasks.py
@@ -6,7 +6,7 @@ from RPA.Excel.Files import Files
 
 from models.browser import Browser
 
-OUTPUT_FILE_PATH = "/output"
+# OUTPUT_FILE_PATH = "/output"
 
 table = Tables()
 excel = Files()
@@ -23,8 +23,12 @@ def search_news():
     news_data = Browser(int(input["month"]),input["section"],input["news"])
     table     = output_data(news_data.news)
 
-    payload = create_work_item_payloads(table)
-    save_work_item_payloads(payload)
+    payloads = create_work_item_payloads(table)
+
+    save_work_item_payloads(payloads[0])
+
+    # for payload in payloads:
+    #     save_work_item_payloads(dict(traffic_data=payload))
     
     # payload = []
 
@@ -76,84 +80,3 @@ def output_data(news_data: dict) -> bool:
     # column = [t.key for t in news_data if t not in column]
     
     return table.create_table(data=news_data)
-
-============================================================================
-
-from robocorp import workitems
-from robocorp.tasks import task
-from RPA.HTTP import HTTP
-from RPA.JSON import JSON
-from RPA.Tables import Tables
-
-http = HTTP()
-json = JSON()
-table = Tables()
-
-TRAFFIC_JSON_FILE_PATH = "output/traffic.json"
-
-# JSON data keys
-COUNTRY_KEY = "SpatialDim"
-YEAR_KEY = "TimeDim"
-RATE_KEY = "NumericValue"
-GENDER_KEY = "Dim1"
-
-@task
-def produce_traffic_data():
-    """
-    Inhuman Insurance, Inc. Artificial Intelligence System automation.
-    Produces traffic data work items.
-    """
-    http.download(
-        url="https://github.com/robocorp/inhuman-insurance-inc/raw/main/RS_198.json",
-        target_file=TRAFFIC_JSON_FILE_PATH,
-        overwrite=True,
-    )
-    traffic_data = load_traffic_data_as_table()
-    filtered_data = filter_and_sort_traffic_data(traffic_data)
-    filtered_data = get_latest_data_by_country(filtered_data)
-    payloads = create_work_item_payloads(filtered_data)
-    save_work_item_payloads(payloads)
-
-@task
-def consume_traffic_data():
-    """
-    Inhuman Insurance, Inc. Artificial Intelligence System automation.
-    Consumes traffic data work items.
-    """
-    print("consume")
-
-def load_traffic_data_as_table():
-    json_data = json.load_json_from_file(TRAFFIC_JSON_FILE_PATH)
-    return table.create_table(json_data["value"])
-
-def filter_and_sort_traffic_data(data):
-    max_rate = 5.0
-    both_genders = "BTSX"
-    table.filter_table_by_column(data, RATE_KEY, "<", max_rate)
-    table.filter_table_by_column(data, GENDER_KEY, "==", both_genders)
-    table.sort_table_by_column(data, YEAR_KEY, False)
-    return data
-
-def get_latest_data_by_country(data):
-    data = table.group_table_by_column(data, COUNTRY_KEY)
-    latest_data_by_country = []
-    for group in data:
-        first_row = table.pop_table_row(group)
-        latest_data_by_country.append(first_row)
-    return latest_data_by_country
-
-def create_work_item_payloads(traffic_data):
-    payloads = []
-    for row in traffic_data:
-        payload = dict(
-            country=row[COUNTRY_KEY],
-            year=row[YEAR_KEY],
-            rate=row[RATE_KEY],
-        )
-        payloads.append(payload)
-    return payloads
-
-def save_work_item_payloads(payloads):
-    for payload in payloads:
-        variables = dict(traffic_data=payload)
-        workitems.outputs.create(variables)
